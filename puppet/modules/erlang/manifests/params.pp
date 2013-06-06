8fd446d44fbcffef092602983a2670b8bd14981b
Roman Heinrich <roman.heinrich@gmail.com>
2013-06-06 19:42:10 +0200

    0.9.1 for elixir lang

--- puppet/modules/erlang/manifests/params.pp
+++ puppet/modules/erlang/manifests/params.pp
@@ -1,5 +1,5 @@
 class erlang::params{
-  $elixir_version = "0.8.2"
+  $elixir_version = "0.9.1"
   $elixir_file    = "v$elixir_version.zip"
   $elixir_folder  = "elixir_$elixir_version"
   $elixir_url     = "http://dl.dropbox.com/u/4934685/elixir/$elixir_file"
